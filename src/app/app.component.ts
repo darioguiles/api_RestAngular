60b03fbb4ed9444e3f7216d5703b421bf00f228b
darioguiles <dario.guilescuesta@hotmail.com>
2024-03-04 11:06:53 +0100

    a

---
+++ src/app/app.component.ts
@@ -0,0 +1,121 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { AppService, Producto, Categoria, Usuario } from './app.service';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  // prueba:Producto[]=[{id:1,nombre:"uno",precio:44.0,categoria:1},
+  //   {id:2,nombre:"dos",precio:55.0,categoria:1},
+  //   {id:3,nombre:"tres",precio:66.0,categoria:1},
+  //   {id:4,nombre:"cuatro",precio:77.0,categoria:1},
+  // ]
+
+  // pruebaSin2():Producto[] {
+  //   this.prueba=[...this.prueba.filter(x => x.id!=2)];
+  //   return this.prueba;
+  // }
+
+  productos:Producto[]=[];
+  categorias: Categoria[]=[];
+  usuarios: Usuario[]=[];
+
+  verAviso:Boolean = false;
+  aviso:string = "";
+
+  constructor(private appService:AppService) {
+    
+  }
+
+  reset(){
+    this.productos=[];
+    this.categorias=[];
+    this.usuarios=[];
+  }
+
+  getProductos() {
+      this.appService.getProductos().subscribe({
+        next: (data) => {
+          this.productos= (data as Producto[])},
+        error: (error) => {
+          //console.error(error);
+          this.setAviso("Error de conexión al servidor.")
+        } 
+      });
+    }
+    getCategorias() {
+      this.appService.getCategorias().subscribe((data) =>
+        this.categorias= (data as any));
+    }
+    getUsuarios() {
+      this.appService.getUsuarios().subscribe((data) =>
+        this.usuarios= (data as any));
+    }
+    getProductosDesde(precio:number) {
+      this.appService.getProductosDesde(precio).subscribe((data) =>
+        this.productos= (data as any));
+    }
+
+    crearProducto(){
+      let producto:Producto={
+        id:99,
+        nombre: "ejemplo",
+        precio: 88.88,
+        categoria: 1
+
+      };
+      // this.appService.crearProducto(producto).subscribe((data) =>
+      //   console.log('crearProducto',data as Producto));
+
+      this.appService.crearProducto(producto).subscribe(
+          {next: () => {
+            console.log("producto creado: ", producto);
+            this.productos.push(producto);
+          },
+          error: (error:HttpErrorResponse) => {
+            let s:string;
+            s=error.error
+            s=s.substring(0,s.indexOf(' at '))
+            this.setAviso(s);
+            console.error(s, error.message);
+          }});
+    }
+    updateProducto(p:Producto, precio:number) {
+      let copiaProducto = {...p};
+      copiaProducto.precio = precio;
+      this.appService.updateProducto(copiaProducto).subscribe({
+        next: (data) =>
+          p.precio = precio,
+        error: error => {
+          this.setAviso('Error: No se ha podido actualizar'); 
+        }
+        });
+    }
+    eliminarProducto(p:Producto) {
+      this.appService.deleteProducto(p.id).subscribe(
+        {next: (() => {
+          //console.log('eliminarProducto');
+          this.quitarDeProductos(p);
+          }),
+        error:  ((error:any) => {
+          //console.log('Error eliminar producto', error);
+          this.setAviso('Error al eliminar producto.')
+        })});
+
+      }
+    quitarDeProductos(p:Producto){
+      this.productos=this.productos.filter(x => x.id!=p.id)
+    }
+
+    setAviso(texto:string){
+      this.aviso=texto;
+      setTimeout(()=> this.aviso="",2000);
+    }
+}
---
+++ src/app/app.service.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
+const PRODUCTOSURL = "http://localhost:3000/productos";
+const PR_BUSCARNOMBREURL = "http://localhost:3000/productos?nombre="
+const PR_DESDEPRECIOURL = "http://localhost:3000/productos?precio_gte="
+const PR_DELETEURL = "http://localhost:3000/productos/"
+const CATEGORIASURL = "http://localhost:3000/categorias";
+const USUARIOSURL = "http://localhost:3000/usuarios";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(private http:HttpClient) {
+   }
+
+   getProductos():Observable<Object>{
+    return this.http.get(PRODUCTOSURL);
+   }
+   getCategorias():Observable<Object>{
+    return this.http.get(CATEGORIASURL);
+   }
+   getUsuarios(){
+    return ;
+   }
+   getProductosDesde(precio:number):Observable<Object>{
+    let cadena:string = PR_DESDEPRECIOURL + precio.toString();
+    // if(!cadena.includes('.')) {
+    //   cadena+= '.0';
+    // } 
+    console.log(cadena);
+    return this.http.get(cadena);
+   }
+   deleteProducto(id:number):Observable<unknown> {
+    const url = `${PR_DELETEURL}/${id}`
+    return this.http.delete(url, httpOptions)
+      .pipe(catchError(this.handleError));
+   }
+   crearProducto(producto:Producto):Observable<Object>{
+    return this.http.post<Producto>(PRODUCTOSURL, producto, httpOptions);
+   }
+   updateProducto(producto:Producto):Observable<Object>{
+    const url = `${PRODUCTOSURL}/${producto.id}`;
+    return this.http.put<Producto>(url, producto, httpOptions);
+   }
+
+   private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+}
+
+export interface Producto {
+  id: number,
+  nombre: string,
+  precio: number,
+  categoria: number
+}
+
+export interface Categoria {
+  id: number,
+  nombre: string
+}
+
+export interface Usuario {
+  id: 2,
+  nombre: string,
+  resultado: any[],
+  passwd: string
+}
